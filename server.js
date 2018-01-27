03dd1e32e6216d7647609bcaa4c447ea97b397d5
David Meyer <david.t.meyer@gmail.com>
2018-01-27 17:34:42 -0500

    specific domain

--- server.js
+++ server.js
@@ -22,7 +22,11 @@ app.get('/service',
     function(req, res) {
         let data = req.param('data')
         console.log(data)
-        res.setHeader("Access-Control-Allow-Origin", "*");
+
+        res.setHeader("Access-Control-Allow-Origin", "https://wmp-dh-dev-ed--c.na73.visual.force.com");        
+        res.setHeader("Vary": "Origin")
+//        res.setHeader("Access-Control-Allow-Origin", "*");
+
         res.setHeader("Access-Control-Allow-Credentials", "true");
         res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
         res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
